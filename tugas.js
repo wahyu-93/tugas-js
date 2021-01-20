770efd3f92bf9e60b93c2d0e0d3923d84c9fdc52
wahyu yudi <wahyu.exam07@gmail.com>
2021-01-21 03:42:43 +0800

    tugas js

---
+++ tugas.js
@@ -0,0 +1,37 @@
+// tugas 1
+var daftarBuah = ["2. Apel", "5. Jeruk", "3. Anggur", "4. Semangka", "1. Mangga"];
+console.log(daftarBuah.sort());
+
+// // tugas 2
+var kalimat="saya sangat senang belajar javascript"
+console.log(kalimat.split(" "));
+
+// // tugas 3
+var Buah = [
+    {
+        nama        : 'strawberry',
+        warna       : 'merah',
+        ada_bijinya : 'tidak',
+        harga       : 9000
+    },
+    {
+        nama        : 'jeruk',
+        warna       : 'oranye',
+        ada_bijinya : 'ada',
+        harga       : 8000
+    },
+    {
+        nama        : 'semangka',
+        warna       : 'hijau & merah',
+        ada_bijinya : 'ada',
+        harga       : 10000
+    },
+    {
+        nama        : 'pisang',
+        warna       : 'kuning',
+        ada_bijinya : 'tidak',
+        harga       : 5000
+    },
+]
+
+console.log(Buah[0]);
